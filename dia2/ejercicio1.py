c69b5fb9454699e4c71d907a7ac98ab860460af5
johan <johanbadillomj@gmail.com>
2025-05-06 10:17:26 -0500

    feat: :sparkles: files new

---
+++ dia2/ejercicio1.py
@@ -0,0 +1,71 @@
+# #######################################
+# ########### Ejercicio Hamburguesas#####
+# #######################################
+
+
+print("Bienvenido a nuestro programa para pedir tus hamburguesas")
+
+
+cantHam = int(input("Cuantas hamburguesas desea pedir: "))
+acumulado=0
+for i in range(cantHam):
+    tipoPan = int(input("Seleccione el tipo de pan 1.para centeno $1000, 2.para avena $1500: "))
+    if tipoPan == 1:
+        print("El tipo de pan es centeno")
+        acumulado = acumulado + 1000
+    elif tipoPan == 2:
+        print("El tipo de pan es avena")
+        acumulado = acumulado + 1500
+
+    tipoCarne= int(input("que tipo de carne deseas: 1.250gr $5000, 2.300gr $7000: "))
+    if tipoCarne ==1:
+        print ("seleccionaste carne de 250gr")
+        acumulado = acumulado + 5000
+    elif tipoCarne ==2:
+        print ("Escogiste la carne de 300gr")
+        acumulado = acumulado + 7000  
+
+    tipoPollo = int(input(" Que cantidad de pollo deseas. 1.250gr $4500, 2.350gr $5500: "))
+    if tipoPollo ==1: 
+        print("Seleccionaste pollo de 250gr")
+        acumulado = acumulado + 4500
+    elif tipoPollo ==2:
+        print("Seleccionaste pollo de 350gr")
+        acumulado=acumulado+5500
+
+    tipoPolloD = int(input("Selecciona la cantidad de pollo Desmechado, 1.250gr $6500, 2.350gr $7500: "))
+    if tipoPolloD==1:
+        print("Seleccionaste pollo desmechado de 250gr")
+        acumulado = acumulado + 6500
+    elif tipoPolloD==2:
+        print("Seleccionaste pollo desmechado de 350gr")
+        acumulado = acumulado + 7500
+
+    tipoTocineta = int (input("Selecciona la cantidad de tocineta, 1.Una lonja $1500, 2.Dos lonjas $2500: "))
+    if tipoTocineta==1:
+        print("Seleccionaste una lonja de tocineta")
+        acumulado = acumulado + 1500
+    elif tipoTocineta==2:
+        print("Seleccionaste dos lonjas de tocineta")
+        acumulado = acumulado + 2500
+
+    tipoPapa = int(input("Selecciona el tipo de papa: 1.francesa $5000, 2.cascos $6000: "))
+    if tipoPapa==1:
+        print("Seleccionaste papa francesa")
+        acumulado = acumulado + 5000
+    elif tipoPapa==2:
+        print("Seleccionaste papa en cascos")
+        acumulado = acumulado + 6000
+
+    tipoBebida = int (input("Selecciona la bebida 1.Gaseosa $5000, 2.Cerveza club $8000, 3.Naranjada $9000: "))
+    if tipoBebida==1:
+        print("Seleccionaste gaseosa")
+        acumulado = acumulado + 5000
+    elif tipoBebida==2:
+        print("Seleccionaste cerveza club")
+        acumulado = acumulado + 8000
+    elif tipoBebida==3:
+        print("Seleccionaste naranjada")
+        acumulado = acumulado + 9000 
+    
+    print("El total con Servicio Incluido es: " , int(acumulado+(acumulado*0.1)))
---
+++ dia2/ejercicio2.py
@@ -0,0 +1,68 @@
+# #######################################
+# ########### Ejercicio Nomina ##########
+# #######################################
+
+
+print("Bienvenido a la organizacion de nominas")
+
+valorHora = 20000
+totalBruto = 0
+totalNeto = 0
+totalEPS = 0
+totalPension = 0  
+
+mayorNeto = 0
+menorNeto = 0
+nombreMayor = ""
+nombreMenor = ""
+
+emple = int(input("Ingrese la cantidad de empleados: "))
+
+for i in range(1, emple+1):
+    print("ngrese el nombre del empleado: ")
+    nombre = input()
+
+    print("Ingrese las horas trabajadas del empleado: ")
+    horas = int(input())
+
+    sueldoBruto = horas * valorHora
+    descuentoEPS = sueldoBruto * 0.04
+    descuentoPension = sueldoBruto * 0.04
+    sueldoNeto = sueldoBruto - descuentoEPS - descuentoPension
+
+    totalBruto = sueldoBruto+totalBruto
+    totalEPS = descuentoEPS+totalEPS
+    totalPension = descuentoPension+totalPension
+    totalNeto = sueldoNeto+totalNeto
+
+    if sueldoNeto > mayorNeto:
+        mayorNeto = sueldoNeto
+        nombreMayor = nombre
+    
+    if sueldoNeto < menorNeto:
+        menorNeto = sueldoNeto
+        nombreMenor = nombre
+
+    print("-----------------------------")
+    print("Empleado:", nombre)
+    print("Sueldo Bruto:", sueldoBruto)
+    print("Descuento EPS:", descuentoEPS)
+    print("Descuento Pensión:", descuentoPension)
+    print("Sueldo Neto:", sueldoNeto)
+    print("-----------------------------")
+
+
+promedioBruto = totalBruto / emple
+promedioNeto = totalNeto / emple
+
+
+
+print("###########################################")
+print("Total Sueldos Brutos:", totalBruto)
+print("Total Descuento EPS:", totalEPS)
+print("Total Descuento Pensión:", totalPension)
+print("Total Sueldos Netos:", totalNeto)
+print("El empleado que más gana es:", nombreMayor, "con un sueldo neto de:", mayorNeto)
+print("El empleado que menos gana es:", nombreMenor, "con un sueldo neto de:", menorNeto)
+print("Promedio de Sueldos Brutos:", promedioBruto)
+print("Promedio de los Sueldos Netos:", promedioNeto)
